65f3c57cf479d5ecef028ae47855a656c4d68423
unknown <xingjiu0228@eyou.com>
2017-10-13 20:28:11 +0800

    xxxxx

--- modules/sysinfo.py
+++ modules/sysinfo.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 import os
 
 def run(**args):
     print "[*] In sysinfo module."
     host_name = os.environ['COMPUTERNAME']
     return str(host_name)
-
-
-=======
-import os
-
-
-host_name = os.environ['COMPUTERNAME']
->>>>>>> 8cea3f937d6c6ee8bc307d096b567a1d2ac754fe
